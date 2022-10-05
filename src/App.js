7603712ee09eb11f001a4806c9930c5f90451425
isir10 <isir.malak@moringaschool.com>
2022-10-05 19:58:16 +0300

    added some styling to page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import bootstrap from "./"
+import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from 'react';
+import MovieList from './components/MovieList';
 
 const App= () => {
-  const[movies, setMovies] =  useState([]);
+  const[movies, setMovies] =  useState([
+    
+  ]);
   return
-    <div>
+    <div className='container-fluid movie-app'>
+      <div className='row'>
+      <MovieList movies={movies}/>
+
+
+
+      </div>
       
     </div>
   
