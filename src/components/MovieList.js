9372d14752b00c9dd5ca1dadea7dd80e75a54d66
isir10 <isir.malak@moringaschool.com>
2022-10-06 22:32:11 +0300

    added overlay effect to y images

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
 const MovieList = (props) => {
-	
-	return 
+
+	return (
+		<>
+			{props.movies.map((movie, index) => (
+				<div className='image-container d-flex justify-content-start m-3'>
+					<img src={movie.Poster} alt='movie'></img>
+					<div className='overlay d-flex align-items-center justify-content-center'>
+					
+					</div>
+				</div>
+			))}
+		</>
+	);
 };
 
 export default MovieList;
