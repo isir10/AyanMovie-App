c0678f9c5687a51e83af4b4e3ad97b40bb42ab8f
isir10 <isir.malak@moringaschool.com>
2022-10-06 15:20:46 +0300

    added a movielistheading component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
 
 const App= () => {
-  const[movies, setMovies] =  useState([
-    
-  ]);
-
+  const[movies, setMovies] =  useState([]);
+  const [Search, setSearchValue] = useState("");
   const getMovieRequest = async () => {
-    const url ="http://www.omdbapi.com/?s=star wars&apikey=baac0f72"
+    const url ="http://www.omdbapi.com/?s=avengers&apikey=baac0f72";
 
     const response = await fetch(url);
     const responseJson = await response.json();
     console.log(responseJson);
+     setMovies(responseJson.Search);
   };
+  useEffect(() => {
+    getMovieRequest();
+  },[]);
+
   return
     <div className='container-fluid movie-app'>
       <div className='row'>
-      <MovieList movies={movies}/>
-
+        <MovieListHeading heading ="movies"/>
+      </div>
+      <div className='row'>
+      <MovieList movies = {movies}/>
+      <h2>hello</h2>
 
 
       </div>
       
     </div>
   
-};
+}; 
 
 export default App;
